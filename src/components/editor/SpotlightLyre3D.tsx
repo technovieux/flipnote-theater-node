06d3f7bb6cb4b041b26fcdc31b54d95c43099f3f
mathiasC <105153480+technovieux@users.noreply.github.com>
2026-06-05 15:55:32 +0300

    Adjust mesh position for volumetric light beam

--- src/components/editor/SpotlightLyre3D.tsx
+++ src/components/editor/SpotlightLyre3D.tsx
@@ -220,7 +220,7 @@ export const SpotlightLyre3D: React.FC<SpotlightLyre3DProps> = ({
           )}
 
           {/* Volumetric light beam (always visible, colored) */}
-          <mesh position={[0, 0.5, 0]} rotation={[Math.PI, 0, 0]}>
+          <mesh position={[0, 1.2, 0]} rotation={[Math.PI, 0, 0]}>
             <coneGeometry args={[0.55, 2.2, 24, 1, true]} />
             <meshBasicMaterial
               color={lightColor}
